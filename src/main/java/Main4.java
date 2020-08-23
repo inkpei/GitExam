794866db11ec1b974f2845a765d6e670cbb9fe35
peilisher <peilisher@gmail.com>
2020-08-23 21:21:32 +0800

    first

---
+++ src/main/java/Main4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        System.out.println(N);
+    }
+}
