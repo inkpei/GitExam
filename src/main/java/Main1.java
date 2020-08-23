e3e1c7bbc6192dafd0e87d38d72fa67216c5466e
peilisher <peilisher@gmail.com>
2020-08-23 20:33:01 +0800

    first

--- src/main/java/Main1.java
+++ src/main/java/Main1.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main1 {
 
     public static void main(String[] args) {
-        System.out.println(":");
-        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            arr.add(scanner.nextInt());
+        }
+        arr.remove(k - 1);
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i));
+            if (i != arr.size()) {
+                System.out.print(" ");
+            }
+
+        }
+        System.out.println();
     }
 }
