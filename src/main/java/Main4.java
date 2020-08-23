9c6a8e30b51affd8c420b76a0861960c369967b6
peilisher <peilisher@gmail.com>
2020-08-23 21:59:30 +0800

    first

--- src/main/java/Main4.java
+++ src/main/java/Main4.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Main4 {
@@ -5,7 +6,22 @@ public class Main4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        System.out.println(N);
-
+        int[] arr = new int[N];
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+            max = Math.max(max, arr[i]);
+            min = Math.min(min, arr[i]);
+        }
+        if (min > N) {
+            System.out.println(N);
+        } else {
+            int ans = 0;
+            for (int i : arr) {
+                if (i == min) ans++;
+            }
+            System.out.println(min + N - ans);
+        }
     }
 }
---
+++ src/main/java/Main5.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main5 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len - i; j++) {
+                if (i == 0) {
+                    dp[j][j + i] = true;
+                } else {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        if (Math.abs(j - i) == 1) {
+                            dp[j][j + i] = true;
+                        } else {
+                            dp[j][j + i] = dp[j + 1][j + i - 1];
+                        }
+                    }
+                }
+
+            }
+        }
+        int N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            int left = scanner.nextInt()-1;
+            int right = scanner.nextInt()-1;
+            int ans = 0;
+            if (dp[left][right]) {
+                ans = 1;
+                System.out.println(1);
+            } else {
+                cmax = Integer.MAX_VALUE;
+                trace(left, right, 0, dp);
+                System.out.println(cmax);
+            }
+        }
+    }
+
+    static int cmax = Integer.MAX_VALUE;
+    static void trace(int a, int b, int k, boolean[][] dp) {
+        if (a > b) {
+            cmax = Math.min(cmax, k);
+            return;
+        }
+        for (int i = a; i <= b; i++) {
+            if (dp[a][i]) {
+                trace(i + 1, b, k + 1, dp);
+            }
+        }
+    }
+}
