643d908f57867ea927522e27a7f0b12fbdefd8cf
peilisher <peilisher@gmail.com>
2020-09-05 15:42:09 +0800

    完美世界

--- src/main/java/完美世界/Main2.java
+++ src/main/java/完美世界/Main2.java
@@ -1,6 +1,22 @@
 package 完美世界;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main2 {
 
+    static int[] arr;
 
+    public static void main(String[] agrs) {
+        Scanner in = new Scanner(System.in);
+        arr = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(judge(0, Long.MIN_VALUE, Long.MAX_VALUE));
+    }
+
+    static boolean judge(int index, long left, long right) {
+        if (index >= arr.length) return true;
+        if (arr[index] <= left || arr[index] >= right) return false;
+        return judge(2 * index + 1, left, arr[index]) && judge(2 * index + 2, arr[index], right);
+    }
 }
+
